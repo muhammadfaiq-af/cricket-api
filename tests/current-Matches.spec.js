767147fd133da4a05429ee89d564ea02c1cfca07
Muhammad Faiq <faiqqadri83@gmail.com>
2024-02-02 17:09:14 +0500

    sa

--- tests/current-Matches.spec.js
+++ tests/current-Matches.spec.js
@@ -4,11 +4,11 @@ test('Current Matches', async ({ request }) => {
 
     const currentMatches = await request.get('https://api.cricapi.com/v1/currentMatches?apikey=700b0f94-86ad-4cb6-8fb9-3be3fb29d71f&offset=0', {
 
-        data: {
+        // data: {
 
-            apikey: '700b0f94-86ad-4cb6-8fb9-3be3fb29d71f',
-            offset: 0
-        }
+        //     apikey: '700b0f94-86ad-4cb6-8fb9-3be3fb29d71f',
+        //     offset: 0
+        // }
     })
 
     const res = await currentMatches.json()
